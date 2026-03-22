f4ccd2f6286a73df7c313782ae1a72dca463ed19
ADMIN <ADMIN@DESKTOP-MPRTCI2>
2026-03-22 13:45:32 +0200

    vsdvsdvsdvs

--- AmitWiessmanWebsite/Manager.aspx
+++ AmitWiessmanWebsite/Manager.aspx
@@ -1,7 +1,26 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Manager.aspx.cs" Inherits="_Default" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="manager.aspx.cs" Inherits="Default9" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
-</asp:Content>
 
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+    
+    <form method="post" runat="Server">
+
+        <h1>מנהל</h1>
+       
+        <label for="fname">:שם פרטי</label><br>
+        <input type="text" id="fname" name="fname"><br>
+
+       
+
+        <input type="submit" value="Submit">
+
+        <br /><br />
+
+       
+
+    </form>
+    <%= st %>
+</asp:Content>
--- AmitWiessmanWebsite/Manager.aspx.cs
+++ AmitWiessmanWebsite/Manager.aspx.cs
@@ -1,1 +1,53 @@
-﻿z
+﻿using System;
+using System.Activities.Expressions;
+using System.Collections.Generic;
+using System.Activities.Statements;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default9 : System.Web.UI.Page
+{
+    public string st = "";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Page.IsPostBack)
+        {
+          string name1 = Request.Form["name"];
+            String sql = "SELECT * FROM tUsers WHERE Name = N'%" + name1 + "'";
+            DataTable dt = MyAdoHelper.ExecuteDataTable( sql);
+            if (dt.Rows.Count > 0)
+            {
+                st = "אין נתונים";
+            }
+            else
+            {
+                st += "<table boarder='1'>'";
+                st += "<tr>";
+                st += "<td>שם פרטי</td>";
+                st += "<td>גיל</td>";
+                st += "<td>סיסמא</td>";
+                st += "<td>מסכים לעדכן</td>";
+
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    st += "<tr>";
+
+                    for (int k = 0; k < dt.Columns.Count; k++)
+                    {
+                        st += "<td>" + dt.Rows[i][k] + "</td>";
+                    }
+                    st += "</tr>";
+                }
+                st += "</table>";
+            }
+        }
+    }
+}
+            
+        
+
+    
