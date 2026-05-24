fb54f7db87e7600f11e280f0938dfe4b89e954ee
User <User@302-07>
2026-05-24 14:43:00 +0300

    כק'ככ'קכ

--- AmitWiessmanWebsite/simpleSerch.aspx
+++ AmitWiessmanWebsite/simpleSerch.aspx
@@ -3,5 +3,25 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+   
+     <form name="formPage"
+     method="post"
+     runat="server"
+   >
+
+    שם השיר:
+     <input type="text"
+         name="songName"
+         id="songName"
+         placeholder="example"
+         required />
+            <input id="Submit1"
+        type="submit"
+        value="שלח" />
+
+</form>
+        <%= st %>
+
 </asp:Content>
 
--- AmitWiessmanWebsite/simpleSerch.aspx.cs
+++ AmitWiessmanWebsite/simpleSerch.aspx.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Activities.Expressions;
 using System.Collections.Generic;
+using System.Activities.Statements;
+using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,8 +11,46 @@ using System.Web.UI.WebControls;
 
 public partial class simpleSerch : System.Web.UI.Page
 {
+    public string st = "";
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (Page.IsPostBack)
+        {
+            string songName = Request.Form["songName"];
 
+            //String sql = "SELECT * FROM tUsers WHERE firstName = N'" + name1 + "'";
+            String sql = "SELECT * FROM ttable WHERE songName LIKE N'%" + songName + "%'";
+
+            DataTable dt = MyAdoHelper.ExecuteDataTable(sql);
+            if (dt.Rows.Count == 0)
+            {
+                st = "אין נתונים";
+            }
+            else
+            {
+                st += "<table boarder='1'>'";
+                st += "<tr>";
+                st += "<td>שם השיר</td>";
+                st += "<td>סוג השיר</td>";
+                st += "<td>אורך השיר בשניות</td>";
+              
+
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    st += "<tr>";
+
+                    for (int k = 0; k < dt.Columns.Count; k++)
+                    {
+                        st += "<td>" + dt.Rows[i][k] + "</td>";
+                    }
+                    st += "</tr>";
+                }
+                st += "</table>";
+            }
+        }
     }
 }
+
+
+
+
